364c0595861f959636ef45fd1bdab633020e1a68
Turky A. Saderaldin <tag2000sa@gmail.com>
2020-03-18 13:58:34 +0300

    upload script that extract mp3 audio from videos using ffmpeg

---
+++ mp4-to-mp3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# File-Name: mp4-to-mp3.sh
+# This file was created on 03/18/20
+# Signature: 202002.ORIGINAL
+# Author: Turky Abdulaziz Saderaldin
+# Subject: script to extract mp3 from mp4
+
+# check inputs
+if [ $# -lt 1 ]
+then
+    echo -e "USAGE: mp4-to-mp3 <file-name>.mp4"
+fi
+
+
+for file in "$@"
+do
+    temp_filename=$(echo $file|sed 's/ /_/g') # set a temporary file name
+    mv -T "$file" $temp_filename # change file name temporarly
+    newFile_name=$(echo $temp_filename|sed "s/.mp4/.mp3/") # extract file name 
+	## convert .mp4 to .mp3
+    ffmpeg -i $temp_filename -vn \
+           -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 -loglevel -8\
+            $newFile_name 
+    mv -T $temp_filename "$file" # restore original file name
+	echo -e "$file -> $newFile_name"
+done
